ae877d0354506523286178b132aebfd8131936c7
GalacticCapybara <galactic.capybara@gmail.com>
2022-01-30 23:55:17 -0300

    chore: update version

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import exportCommand from "./export";
 import importCommand from "./import";
 
 const options = yargs
-  .version("0.1.3")
+  .version("0.1.4")
   .usage(
     "Usage: torchlight-3-save-editor [command] [option]\n" +
       "\n" +
